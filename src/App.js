6fb4695841227d230d641fcb40b2a2dee25d18cb
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-02 22:05:36 -0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          class="App-link"
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return <div class="App">Calculator App</div>;
 }
 
 export default App;
