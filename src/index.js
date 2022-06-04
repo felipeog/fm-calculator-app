1cdefa0cf0e7d69fdac7394cfbdbc89a9cb8ddf8
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-04 01:03:35 -0300

    wip: css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { render } from "solid-js/web";
-
+import "./css/reset.css";
+import "./css/base.css";
+import "./css/theme.css";
 import App from "./App";
 
 render(App, document.getElementById("root"));
