1327ee058b18ba8130abda3a8c0ce60e4d82f8b0
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-04 00:28:50 -0300

    complete todos

--- src/stores/calculator.js
+++ src/stores/calculator.js
@@ -17,50 +17,54 @@ const [calculator, setCalculator] = createStore({
 });
 
 export function handleKeyboardInput(input) {
-  if (typeof input === "number") {
-    setCalculator((prevCalculator) => {
-      const result = handleNumber(input, prevCalculator);
-      console.log("handleNumber", JSON.stringify(result, null, 2));
-      return result;
-    });
-  }
+  let result;
 
-  if (typeof input === "string") {
-    let result;
+  setCalculator((prevCalculator) => {
+    switch (input) {
+      case "del":
+        return handleDelete(prevCalculator);
 
-    setCalculator((prevCalculator) => {
-      switch (input) {
-        case "del":
-          return handleDelete(prevCalculator);
+      case ".":
+        return handleDecimal(input, prevCalculator);
 
-        case "+":
-        case "-":
-        case "/":
-        case "*":
-          result = handleOperation(input, prevCalculator);
-          console.log("handleOperation", JSON.stringify(result, null, 2));
-          return result;
+      case "reset":
+        return handleReset();
 
-        case ".":
-          return handleDecimal(input, prevCalculator);
+      case "=":
+        result = handleEquals(prevCalculator);
+        console.log("handleEquals", JSON.stringify(result, null, 2));
+        return result;
 
-        case "reset":
-          return handleReset();
+      case "+":
+      case "-":
+      case "/":
+      case "*":
+        result = handleOperation(input, prevCalculator);
+        console.log("handleOperation", JSON.stringify(result, null, 2));
+        return result;
 
-        case "=":
-          result = handleEquals(prevCalculator);
-          console.log("handleEquals", JSON.stringify(result, null, 2));
-          return result;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        result = handleNumber(input, prevCalculator);
+        console.log("handleNumber", JSON.stringify(result, null, 2));
+        return result;
 
-        default:
-          return prevCalculator;
-      }
-    });
-  }
+      default:
+        return prevCalculator;
+    }
+  });
 }
 
 function handleNumber(input, { currentValue }) {
-  if (currentValue === "0" && String(input) === "0") {
+  if (currentValue === "0" && input === "0") {
     return {
       isReadingValue: true,
     };
@@ -68,23 +72,23 @@ function handleNumber(input, { currentValue }) {
 
   return {
     isReadingValue: true,
-    currentValue: currentValue + String(input),
+    currentValue: currentValue + input,
   };
 }
 
-// TODO:
 function handleDelete({ currentValue }) {
-  // return {
-  //   currentValue: currentValue.slice(0, -1),
-  //   displayContent: currentValue.slice(0, -1),
-  // };
+  return {
+    currentValue:
+      currentValue.length > 0 ? currentValue.slice(0, -1) : currentValue,
+  };
 }
 
-// TODO:
 function handleDecimal(input, { currentValue }) {
-  // return {
-  //   currentValue: currentValue + input,
-  // };
+  return {
+    currentValue: currentValue.includes(".")
+      ? currentValue
+      : currentValue + input,
+  };
 }
 
 function handleReset() {
