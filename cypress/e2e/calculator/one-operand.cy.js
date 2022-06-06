e373b2b01487f79755c835cd836c5c72a180e6d2
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-05 21:35:24 -0300

    wip

---
+++ cypress/e2e/calculator/one-operand.cy.js
@@ -0,0 +1,125 @@
+/// <reference types="cypress" />
+
+describe("operand/operation", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("sums", () => {
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("+");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("4");
+
+    cy.pressButton("=");
+    cy.checkDisplay("6");
+  });
+
+  it("subtracts", () => {
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("-");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0");
+
+    cy.pressButton("=");
+    cy.checkDisplay("-2");
+  });
+
+  it("multiplies", () => {
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("x");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("4");
+
+    cy.pressButton("=");
+    cy.checkDisplay("8");
+  });
+
+  it("divides", () => {
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("/");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("1");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0.5");
+  });
+});
+
+describe("operation/operand", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("sums", () => {
+    cy.pressButton("+");
+    cy.checkDisplay("");
+
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("4");
+  });
+
+  it("subtracts", () => {
+    cy.pressButton("-");
+    cy.checkDisplay("");
+
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("-2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("-4");
+  });
+
+  it("multiplies", () => {
+    cy.pressButton("x");
+    cy.checkDisplay("");
+
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0");
+  });
+
+  it("divides", () => {
+    cy.pressButton("/");
+    cy.checkDisplay("");
+
+    cy.pressButton("2");
+    cy.checkDisplay("2");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0");
+
+    cy.pressButton("=");
+    cy.checkDisplay("0");
+  });
+});
