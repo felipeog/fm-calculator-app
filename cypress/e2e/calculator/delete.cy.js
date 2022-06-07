7b9474ae8778033c767d7e38f632edb8888ea62f
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-07 19:04:05 -0300

    wip

--- cypress/e2e/calculator/delete.cy.js
+++ cypress/e2e/calculator/delete.cy.js
@@ -7,14 +7,32 @@ describe("delete", () => {
 
   it("deletes one character at a time", () => {
     cy.pressButton("0");
+    cy.checkDisplay("0");
+
     cy.pressButton("1");
+    cy.checkDisplay("1");
+
     cy.pressButton("2");
+    cy.checkDisplay("12");
+
     cy.pressButton("3");
+    cy.checkDisplay("123");
+
     cy.pressButton("4");
+    cy.checkDisplay("1,234");
+
     cy.pressButton("5");
+    cy.checkDisplay("12,345");
+
     cy.pressButton("6");
+    cy.checkDisplay("123,456");
+
     cy.pressButton("7");
+    cy.checkDisplay("1,234,567");
+
     cy.pressButton("8");
+    cy.checkDisplay("12,345,678");
+
     cy.pressButton("9");
     cy.checkDisplay("123,456,789");
 
