6bb4e4c48d2f8e8094f3f01f2befb13ed70ec1e5
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-07 23:14:43 -0300

    wip

--- cypress/e2e/calculator/boundaries.cy.js
+++ cypress/e2e/calculator/boundaries.cy.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
-// TODO:
-describe.skip("boundaries", () => {
+describe("boundaries", () => {
   before(() => {
     cy.visit("/");
   });
@@ -10,7 +9,65 @@ describe.skip("boundaries", () => {
     cy.pressButton("reset");
   });
 
-  it("boundaries", () => {
-    // boundaries
+  it("should not surpass upper boundary", () => {
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999");
+
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999,999");
+
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999,999,999");
+
+    cy.pressButton("1");
+    cy.checkDisplay("999,999,999");
+
+    cy.pressButton("1");
+    cy.checkDisplay("999,999,999");
+  });
+
+  it("should not surpass lower boundary", () => {
+    cy.pressButton("-");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999");
+
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999,999");
+
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.pressButton("9");
+    cy.checkDisplay("999,999,999");
+
+    cy.pressButton("1");
+    cy.checkDisplay("999,999,999");
+
+    cy.pressButton("1");
+    cy.checkDisplay("999,999,999");
+  });
+
+  it("should not surpass decimal precision", () => {
+    cy.pressButton(".");
+    cy.pressButton("1");
+    cy.pressButton("2");
+    cy.pressButton("3");
+    cy.pressButton("4");
+    cy.checkDisplay("0.1234");
+
+    cy.pressButton("5");
+    cy.checkDisplay("0.1234");
+
+    cy.pressButton("6");
+    cy.checkDisplay("0.1234");
   });
 });
