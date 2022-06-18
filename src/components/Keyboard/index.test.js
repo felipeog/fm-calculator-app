697beb37b2cb716c77697379a31329b85154dc16
felipeog <17603069+felipeog@users.noreply.github.com>
2022-06-17 21:40:23 -0300

    Add Keyboard tests

---
+++ src/components/Keyboard/index.test.js
@@ -0,0 +1,123 @@
+import { render, fireEvent } from "solid-testing-library";
+
+import { Keyboard } from "./";
+import calculatorStore from "../../stores/calculator";
+import transitionStore from "../../stores/transition";
+
+jest.mock("../../stores/calculator", () => {
+  return {
+    ...jest.requireActual("../../stores/calculator"),
+    handleKeyboardInput: () => {},
+  };
+});
+jest.mock("../../stores/transition", () => {
+  return {
+    ...jest.requireActual("../../stores/transition"),
+    handleDisplayTransition: () => {},
+    handleKeyboardTransition: () => {},
+  };
+});
+
+const buttonLabels = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "+",
+  "-",
+  "x",
+  "/",
+  ".",
+  "=",
+  "reset",
+  "del",
+];
+const buttonKeys = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "+",
+  "-",
+  "x",
+  "/",
+  ".",
+  "=",
+  "r",
+  "Backspace",
+  "Delete",
+];
+
+describe("Keyboard", () => {
+  beforeEach(() => {
+    calculatorStore.handleKeyboardInput = () => {};
+    transitionStore.handleDisplayTransition = () => {};
+    transitionStore.handleKeyboardTransition = () => {};
+  });
+
+  it("renders all buttons", () => {
+    const { getByText, unmount } = render(() => <Keyboard />);
+
+    buttonLabels.forEach((button) => {
+      expect(getByText(button)).toBeInTheDocument();
+    });
+
+    unmount();
+  });
+
+  it("triggers callbacks on click", () => {
+    calculatorStore.handleKeyboardInput = jest.fn();
+    transitionStore.handleDisplayTransition = jest.fn();
+
+    const { queryByText, unmount } = render(() => <Keyboard />);
+
+    buttonLabels.forEach((buttonLabel) => {
+      fireEvent.click(queryByText(buttonLabel));
+
+      expect(calculatorStore.handleKeyboardInput).toHaveBeenCalledWith(
+        buttonLabel
+      );
+      expect(transitionStore.handleDisplayTransition).toHaveBeenCalledWith(
+        buttonLabel
+      );
+    });
+
+    unmount();
+  });
+
+  it("triggers callbacks on keydown", () => {
+    calculatorStore.handleKeyboardInput = jest.fn();
+    transitionStore.handleDisplayTransition = jest.fn();
+    transitionStore.handleKeyboardTransition = jest.fn();
+
+    const { unmount } = render(() => <Keyboard />);
+
+    buttonKeys.forEach((buttonKey) => {
+      fireEvent.keyDown(document, { key: buttonKey });
+
+      expect(calculatorStore.handleKeyboardInput).toHaveBeenCalledWith(
+        buttonKey
+      );
+      expect(transitionStore.handleDisplayTransition).toHaveBeenCalledWith(
+        buttonKey
+      );
+      expect(transitionStore.handleKeyboardTransition).toHaveBeenCalledWith(
+        buttonKey
+      );
+    });
+
+    unmount();
+  });
+});
